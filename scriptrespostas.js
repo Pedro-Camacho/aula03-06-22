7000dab643d3a4e243f16eba260ec5486232a7f5
PEDRO ALMEIDA CAMACHO <pedro.acamacho@senacsp.edu.br>
2022-06-03 21:08:27 -0300

    commit

--- scriptrespostas.js
+++ scriptrespostas.js
@@ -1,9 +1,14 @@
 let score = 0;
+function acertou(){
+	let correto = document.getElementById("corpo");
+	
+}
+
 function loadperguntas(){
    let resposta =document.querySelector("#score");
 		resposta.innerText=score;
 }
-function start(){
+function init(){
    let primeirapergunta=document.getElementById("questionario");
    primeirapergunta.style.display="block";
    let limpartela=document.getElementById("startbttn");
@@ -15,7 +20,7 @@ function responder1(valor){
    let resposta =document.querySelector("#score");
 		resposta.innerText=score;
     if(valor==1){
-       alert("acertou");
+       acertou();
        score++;
       resposta.innerText=score; 
     }  
@@ -23,41 +28,111 @@ function responder1(valor){
     ultima.style.display="none";
     
 }
-
 function responder2(valor){
-   let next= document.getElementById("pergunta3");
+   let next= document.getElementById("pergunta2" );
    next.style.display="block";
    let resposta =document.querySelector("#score");
 		resposta.innerText=score;
     if(valor==1){
-       alert("acertou");
+       acertou();
        score++;  
     }  
-    let ultima=document.getElementById("pergunta2");
+    let ultima=document.getElementById("pergunta1");
     ultima.style.display="none";
 }
-
 function responder3(valor){
    let next= document.getElementById("pergunta4");
    next.style.display="block";
    let resposta =document.querySelector("#score");
 		resposta.innerText=score;
     if(valor==1){
-       alert("acertou");
+       acertou();
        score++;  
     }  
     let ultima=document.getElementById("pergunta3");
     ultima.style.display="none";
 }
 function responder4(valor){
-   let next= document.getElementById("pergunta4");
+   let next= document.getElementById("pergunta5");
    next.style.display="block";
    let resposta =document.querySelector("#score");
 		resposta.innerText=score;
     if(valor==1){
-       alert("acertou");
+       acertou();
        score++;  
     }  
-    let ultima=document.getElementById("pergunta3");
+    let ultima=document.getElementById("pergunta4");
+    ultima.style.display="none";
+}
+function responder5(valor){
+   let next= document.getElementById("pergunta6");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       acertou();
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta5");
+    ultima.style.display="none";
+}
+function responder6(valor){
+   let next= document.getElementById("pergunta7");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       acertou();
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta6");
+    ultima.style.display="none";
+}
+function responder7(valor){
+   let next= document.getElementById("pergunta8");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+	   acertou();
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta7");
+    ultima.style.display="none";
+}
+function responder8(valor){
+   let next= document.getElementById("pergunta9");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       acertou();
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta8");
+    ultima.style.display="none";
+}
+function responder9(valor){
+   let next= document.getElementById("pergunta10");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       acertou();
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta9");
+    ultima.style.display="none";
+}
+function responder10(valor){
+   let next= document.getElementById("fimquiz");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       acertou();
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta10");
     ultima.style.display="none";
 }
